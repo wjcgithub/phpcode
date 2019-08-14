0aba8d674c6b86082df77c37961a709ead7c2938
wjc <732578448@qq.com>
2019-08-14 19:50:53 +0800

    php

---
+++ php_source/variables/zval.php
@@ -0,0 +1,14 @@
+<?php
+$a = "123";
+echo $a;
+$b = true;
+echo $b;
+$c = 500;
+echo $c;
+//$d = [1,2,3];
+//echo $d;
+//$e = null;
+//echo $e;
+//$std = new stdClass();
+//$std->name = "zhangsan";
+//echo $std;
