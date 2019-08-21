a27f187a435e420faa4776e33a400ce223a5e879
wjc <732578448@qq.com>
2019-08-21 14:12:21 +0800

    php src parse

--- php_source/variables/arr/arr2.php
+++ php_source/variables/arr/arr2.php
@@ -4,3 +4,5 @@ $a[1] = "a";
 echo $a[1];
 $a[100] = "d";
 echo $a[100];
+//ZEND_ECHO_SPEC_CV_HANDLER
+//p (int32_t)z.value.arr.nTableMask
--- php_source/variables/arr/packed_array.php
+++ php_source/variables/arr/packed_array.php
@@ -8,3 +8,15 @@ for ($i=0; $i<=200000; $i++) {
 }
 
 echo memory_get_usage() - $memory_start, " bytes\n";
+
+
+
+$memory_start = memory_get_usage();
+
+$test1 = [];
+
+for ($i=4; $i<=200000; $i++) {
+    $test1[$i] = 1;
+}
+
+echo memory_get_usage() - $memory_start, " bytes\n";
